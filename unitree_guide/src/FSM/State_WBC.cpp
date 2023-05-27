c42ed6fe26d9d9ec923fad13181d9e9bad5606ae
fan-ziqi <fanziqi614@gmail.com>
2023-05-27 14:41:04 +0800

    wbc

---
+++ unitree_guide/src/FSM/State_WBC.cpp
@@ -0,0 +1,247 @@
+#include "FSM/State_WBC.h"
+#include <iomanip>
+
+State_WBC::State_WBC(CtrlComponents *ctrlComp)
+		: FSMState(ctrlComp, FSMStateName::WBC, "wbc"),
+		  _est(ctrlComp->estimator), _phase(ctrlComp->phase),
+		  _contact(ctrlComp->contact), _robModel(ctrlComp->robotModel),
+		  _balCtrl(ctrlComp->balCtrl)
+{
+	// 开启安全检测
+	this->checkSafeOrientation = true;
+
+	_gait = new GaitGenerator(ctrlComp);
+
+	_gaitHeight = 0.08;
+
+#ifdef ROBOT_TYPE_Go1
+	_Kpp = Vec3(70, 70, 70).asDiagonal();
+	_Kdp = Vec3(10, 10, 10).asDiagonal();
+	_kpw = 780;
+	_Kdw = Vec3(70, 70, 70).asDiagonal();
+	_KpSwing = Vec3(400, 400, 400).asDiagonal();
+	_KdSwing = Vec3(10, 10, 10).asDiagonal();
+#endif
+
+#ifdef ROBOT_TYPE_A1
+	_Kpp = Vec3(20, 20, 100).asDiagonal();
+	_Kdp = Vec3(20, 20, 20).asDiagonal();
+	_kpw = 400;
+	_Kdw = Vec3(50, 50, 50).asDiagonal();
+	_KpSwing = Vec3(400, 400, 400).asDiagonal();
+	_KdSwing = Vec3(10, 10, 10).asDiagonal();
+#endif
+
+#ifdef ROBOT_TYPE_CYBERDOG
+	_Kpp = Vec3(70, 70, 70).asDiagonal();        // 位置刚度 在保持平衡的前提下尽量缩小
+	_Kdp = Vec3(10, 10, 10).asDiagonal();        // 位置阻尼
+	_kpw = 500;                                  // 姿态刚度 在保持平衡的前提下尽量缩小
+	_Kdw = Vec3(50, 50, 50).asDiagonal();        // 姿态阻尼
+	_KpSwing = Vec3(400, 400, 400).asDiagonal(); // 足端刚度
+	_KdSwing = Vec3(25, 25, 25).asDiagonal();    // 足端阻尼
+#endif
+
+	_vxLim = _robModel->getRobVelLimitX();
+	_vyLim = _robModel->getRobVelLimitY();
+	_wyawLim = _robModel->getRobVelLimitYaw();
+
+}
+
+State_WBC::~State_Trotting()
+{
+	delete _gait;
+}
+
+void State_WBC::enter()
+{
+	_pcd = _est->getPosition();
+	_pcd(2) = -_robModel->getFeetPosIdeal()(2, 0);
+	_vCmdBody.setZero();
+	_yawCmd = _lowState->getYaw();
+	_Rd = rotz(_yawCmd);
+	_wCmdGlobal.setZero();
+
+	_ctrlComp->ioInter->zeroCmdPanel();
+	_gait->restart();
+}
+
+void State_WBC::exit()
+{
+	_ctrlComp->ioInter->zeroCmdPanel();
+	_ctrlComp->setAllSwing();
+}
+
+FSMStateName State_WBC::checkChange()
+{
+	if(_lowState->userCmd == UserCommand::L2_B)
+	{
+		return FSMStateName::PASSIVE;
+	}
+	else if(_lowState->userCmd == UserCommand::L2_A)
+	{
+		return FSMStateName::FIXEDSTAND;
+	}
+	else
+	{
+		return FSMStateName::WBC;
+	}
+}
+
+void State_WBC::run()
+{
+	_posBody = _est->getPosition();
+	_velBody = _est->getVelocity();
+	_posFeet2BGlobal = _est->getPosFeet2BGlobal();
+	_posFeetGlobal = _est->getFeetPos();
+	_velFeetGlobal = _est->getFeetVel();
+	_B2G_RotMat = _lowState->getRotMat();
+	_G2B_RotMat = _B2G_RotMat.transpose();
+	_yaw = _lowState->getYaw();
+	_dYaw = _lowState->getDYaw();
+
+	_userValue = _lowState->userValue;
+
+	getUserCmd();
+	calcCmd();
+
+	_gait->setGait(_vCmdGlobal.segment(0, 2), _wCmdGlobal(2), _gaitHeight);
+	_gait->run(_posFeetGlobalGoal, _velFeetGlobalGoal);
+
+	calcTau();
+	calcQQd();
+
+	if(checkStepOrNot())
+	{
+		_ctrlComp->setStartWave();
+	}
+	else
+	{
+		_ctrlComp->setAllStance();
+	}
+
+	_lowCmd->setTau(_tau);
+	_lowCmd->setQ(vec34ToVec12(_qGoal));
+	_lowCmd->setQd(vec34ToVec12(_qdGoal));
+
+	for(int i(0); i < 4; ++i)
+	{
+		if((*_contact)(i) == 0)
+		{
+			_lowCmd->setSwingGain(i);
+		}
+		else
+		{
+			_lowCmd->setStableGain(i);
+		}
+	}
+
+}
+
+bool State_WBC::checkStepOrNot()
+{
+	if((fabs(_vCmdBody(0)) > 0.03) ||
+	   (fabs(_vCmdBody(1)) > 0.03) ||
+	   (fabs(_posError(0)) > 0.08) ||
+	   (fabs(_posError(1)) > 0.08) ||
+	   (fabs(_velError(0)) > 0.05) ||
+	   (fabs(_velError(1)) > 0.05) ||
+	   (fabs(_dYawCmd) > 0.20))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void State_WBC::setHighCmd(double vx, double vy, double wz)
+{
+	_vCmdBody(0) = vx;
+	_vCmdBody(1) = vy;
+	_vCmdBody(2) = 0;
+	_dYawCmd = wz;
+}
+
+void State_WBC::getUserCmd()
+{
+	/* Movement */
+	_vCmdBody(0) = invNormalize(_userValue.ly, _vxLim(0), _vxLim(1));
+	_vCmdBody(1) = -invNormalize(_userValue.lx, _vyLim(0), _vyLim(1));
+	_vCmdBody(2) = 0;
+
+	/* Turning */
+	_dYawCmd = -invNormalize(_userValue.rx, _wyawLim(0), _wyawLim(1));
+	_dYawCmd = 0.9 * _dYawCmdPast + (1 - 0.9) * _dYawCmd;
+	_dYawCmdPast = _dYawCmd;
+}
+
+void State_WBC::calcCmd()
+{
+	/* Movement */
+	_vCmdGlobal = _B2G_RotMat * _vCmdBody;
+
+	_vCmdGlobal(0) = saturation(_vCmdGlobal(0), Vec2(_velBody(0) - 0.2, _velBody(0) + 0.2));
+	_vCmdGlobal(1) = saturation(_vCmdGlobal(1), Vec2(_velBody(1) - 0.2, _velBody(1) + 0.2));
+
+	_pcd(0) = saturation(_pcd(0) + _vCmdGlobal(0) * _ctrlComp->dt, Vec2(_posBody(0) - 0.05, _posBody(0) + 0.05));
+	_pcd(1) = saturation(_pcd(1) + _vCmdGlobal(1) * _ctrlComp->dt, Vec2(_posBody(1) - 0.05, _posBody(1) + 0.05));
+
+	_vCmdGlobal(2) = 0;
+
+	/* Turning */
+	_yawCmd = _yawCmd + _dYawCmd * _ctrlComp->dt;
+
+	_Rd = rotz(_yawCmd);
+	_wCmdGlobal(2) = _dYawCmd;
+}
+
+void State_WBC::calcTau()
+{
+	_posError = _pcd - _posBody;
+	_velError = _vCmdGlobal - _velBody;
+
+	_ddPcd = _Kpp * _posError + _Kdp * _velError;
+	_dWbd = _kpw * rotMatToExp(_Rd * _G2B_RotMat) + _Kdw * (_wCmdGlobal - _lowState->getGyroGlobal());
+
+	_ddPcd(0) = saturation(_ddPcd(0), Vec2(-3, 3));
+	_ddPcd(1) = saturation(_ddPcd(1), Vec2(-3, 3));
+	_ddPcd(2) = saturation(_ddPcd(2), Vec2(-5, 5));
+
+	_dWbd(0) = saturation(_dWbd(0), Vec2(-40, 40));
+	_dWbd(1) = saturation(_dWbd(1), Vec2(-40, 40));
+	_dWbd(2) = saturation(_dWbd(2), Vec2(-10, 10));
+
+	_forceFeetGlobal = -_balCtrl->calF(_ddPcd, _dWbd, _B2G_RotMat, _posFeet2BGlobal, *_contact);
+
+	for(int i(0); i < 4; ++i)
+	{
+		if((*_contact)(i) == 0)
+		{
+			_forceFeetGlobal.col(i) =
+					_KpSwing * (_posFeetGlobalGoal.col(i) - _posFeetGlobal.col(i)) + _KdSwing * (_velFeetGlobalGoal.col(i) - _velFeetGlobal.col(i));
+		}
+	}
+
+	_forceFeetBody = _G2B_RotMat * _forceFeetGlobal;
+	_q = vec34ToVec12(_lowState->getQ());
+	_tau = _robModel->getTau(_q, _forceFeetBody);
+}
+
+void State_WBC::calcQQd()
+{
+
+	Vec34 _posFeet2B;
+	_posFeet2B = _robModel->getFeet2BPositions(*_lowState, FrameType::BODY);
+
+	for(int i(0); i < 4; ++i)
+	{
+		_posFeet2BGoal.col(i) = _G2B_RotMat * (_posFeetGlobalGoal.col(i) - _posBody);
+		_velFeet2BGoal.col(i) = _G2B_RotMat * (_velFeetGlobalGoal.col(i) - _velBody);
+		// _velFeet2BGoal.col(i) = _G2B_RotMat * (_velFeetGlobalGoal.col(i) - _velBody - _B2G_RotMat * (skew(_lowState->getGyro()) * _posFeet2B.col(i)) );  //  c.f formula (6.12)
+	}
+
+	_qGoal = vec12ToVec34(_robModel->getQ(_posFeet2BGoal, FrameType::BODY));
+	_qdGoal = vec12ToVec34(_robModel->getQd(_posFeet2B, _velFeet2BGoal, FrameType::BODY));
+}
+
